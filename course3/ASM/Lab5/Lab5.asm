be336650239dc5f9451e2c81f574eca612042609
UnShame <mrunshame@gmail.com>
2018-11-04 00:58:50 +0300

    ASM Lab5: print biggest file's name and size

--- course3/ASM/Lab5/Lab5.asm
+++ course3/ASM/Lab5/Lab5.asm
@@ -2,14 +2,17 @@
 .stack 200h
 
 .data
-    buf db 512*15 dup (?)
-    mess1 db 0ah, 0dh, '$' 
-    nomber db 0
-    mess2 db 'No files starting with t', 0ah, 0dh, '$'
-    mess3 db 'Press whatever$'
+    buff db 512*15 dup (?)                          ; буфер для информации о каталоге
+    endl db 0ah, 0dh, '$'                           ; символы конца строки
+    numfiles db 0                                   ; счетчик кол-ва файлов в каталоге
+    msg_nofiles db 'No files found', 0ah, 0dh, '$'  ; сообщение о том, что нет файлов
+    filename db 12 dup (?)                          ; имя наибольшего файла
+    filesizeh dw 0                                  ; верхние 2 байта размера наиб. файла
+    filesizel dw 0                                  ; нижние 2 байта размера наиб. файла
 
 .code
 
+; выводит 16бит число в десятичном виде
 print_dec proc c
 arg input:word
 
@@ -18,54 +21,50 @@ arg input:word
     push cx
     push dx
 
-    ;Set up registers:
-    ;AX = Number left to print
-    ;BX = Power of ten to extract the current digit
-    ;DX = Scratch/Needed for DIV
-    ;CX = Scratch
-
+    ; Используемые регистры:
+    ; AX = еще не выведенное число
+    ; BX = степень десяти для вытаскивания текущей цифры
+    ; DX = нужно для div
+    ; CX = остаток от div
     mov ax, input
     mov bx, 10000d
     xor dx, dx
 
     _pd_convert:     
-        div bx                           ;DX = Number without highmost digit, AX = Highmost digit
-        mov cx, dx                       ;Number left to print
+        div bx      ; DX = число без верхней цифры, AX = верхняя цифра
+        mov cx, dx  ; еще невыведенное число
 
-        ;If digit is non zero or param for leading zeros is non zero
-        ;print the digit
+        ; если цифра не ноль, то выводим ее
         cmp ax, 0
         jnz _pd_print
 
-        ;If both are zeros, make sure to show at least one digit so that 0 prints as "0"
+        ; если все число равно нулю, то выводим ноль
         cmp bx, 1
-        jne _pd_continue
+    jne _pd_continue
 
     _pd_print:
-
-        ;Print digit in AL
-
+        ; выводим цифру в al
         mov dl, al
         add dl, '0'
         mov ah, 02h
         int 21h
+    jmp _pd_continue
 
     _pd_continue:
-        ;BX = BX/10
-        ;DX = 0
-
+        ; bx = bx/10
+        ; dx = 0
         mov ax, bx
         xor dx, dx
         mov bx, 10d
         div bx
         mov bx, ax
 
-        ;Put what's left of the number in AX again and repeat...
+        ; кладем, что осталось от числа в ax и повторяем...
         mov ax, cx
 
-        ;...Until the divisor is zero
+        ; ...пока делитель не 0
         test bx, bx
-        jnz _pd_convert
+    jnz _pd_convert
 
     pop dx
     pop cx
@@ -75,6 +74,8 @@ arg input:word
     ret
 print_dec endp
 
+; выводит размер в кб
+; input - верхние 2 байта 32бит размера в байтах
 print_kb proc c
 arg input:word
 
@@ -83,12 +84,16 @@ arg input:word
 
     mov ax, input
     mov bx, input
-    shr ax, 2
-    and bx, 10b
+
+    ; сдвигаем, чтобы 0000 0001 == 1кб
+    shr ax, 2  
+    ; округляем вверх, если в бите, значащем 512б, стоит 1
+    and bx, 10b 
     cmp bx, 0
     je _pkb_print
     add ax, 1
 
+    ; выводим xxxkb 
     _pkb_print:
         push ax
         call print_dec
@@ -108,90 +113,135 @@ arg input:word
     ret
 print_kb endp
 
+main:
+    ; ставим сегмент данных
+    mov ax, @data
+    mov ds, ax
 
+    ; считываем корневой каталог в буфер
+    mov dx, 19
+    mov cx ,15
+    mov bx, offset buff
+    mov al, 0
+    int 25h
+    pop bx
+    jc check_count
 
-start:
-mov ax, @data
-mov ds, ax
+    ; настраиваем si на начало буфера
+    mov si, offset buff
 
-begin:
-; считываем корневой каталог в буфер
-mov dx, 19
-mov cx ,15
-mov bx, offset buf
-mov al, 0
-int 25h
-pop bx
-jc exit1
+jmp next_file
 
-; настраиваем si на начало буфера
-mov si, offset buf
+; читаем первый байт очередного элемента каталога
+next_file:
+    mov al, [si]
 
-m2:
-mov al, [si]   ; читаем первый байт очередного элемента каталога
-cmp al, 0       ; файлов больше нет?
-je exit1          ; да, выходим
+    ; файлов больше нет?
+    cmp al, 0      
+    je check_count  
 
-cmp al, 0e5h  ; удаленный файл?
-jne m3            ; нет, проверяем дальше
-add si, 32
-jmp m2          ; раз удален, переходим к следующему элементу каталога
+    ; удаленный файл?
+    cmp al, 0e5h
+    jne check_file_size
 
+    ; след. файл
+    add si, 32
 
-m3:
-; наращиваем счетчик
-inc nomber
+jmp next_file
 
-; выводим название файла на экран, начиная с текущей позиции курсора
-mov cx, 11      ; число повторений цикла (8 – название, 3 – расширение)
-mov ah, 0eh    ; функция
+; сравниваем размер файла
+check_file_size:
+    ; наращиваем счетчик
+    inc numfiles
 
-m4:
-mov al, [si]    ; в al  заносим ASCII – код очередного выводимого символа
-int 10h            ; выводим символ, курсор сам сдвигается на позицию вправо
+    ; запоминаем индекс 
+    push si
+    ;сдвигаем его к позиции с размером файла
+    add si, 29
 
-inc si               ; теперь si адресует следующий выводимый символ
-loop m4  
+    ; сравниваем верхние два байта размера
+    mov ax, [si]
+    cmp ax, filesizeh
+    jg  new_max_size
+    jl  shift_index
 
-mov ah, 02h
-mov dl, ' '
-int 21h
+    ; сравниваем нижние два байта размера
+    mov ax, [si+2]
+    cmp ax, filesizel
+    jg  new_max_size
 
+jmp shift_index
 
-mov ah, 0eh
-add si, 18        ; 11 мы уже прибавили к si в цикле при выводе названия файла
-mov ax, [si]
-push ax
-call print_kb
-pop ax
-add si, 3
+; новый макс. размер найден
+new_max_size:
+    ; запоминаем его
+    mov ax, [si]
+    mov filesizeh, ax
+    mov ax, [si+2]
+    mov filesizel, ax
 
-; переводим курсор в начало следующей строки экрана
-mov ah, 9
-mov dx, offset mess1
-int 21h
+    ; возвращаемся к началу элемента каталога
+    pop si
+    ; снова запоминаем индекс
+    push si
 
-jmp m2           ; переходим к исследованию следующего элемента каталога
+    ; запоминаем имя файла
+    mov bx, 0
+    mov cx, 11      
+    read_char:
+        mov al, [si]
+        mov [filename + bx], al
 
-exit1:
-cmp nomber, 0
-jne exit                ; были файлы, начинающиеся с Т
+        inc si      
+        inc bx
+    loop read_char  
+    mov [filename + bx], '$'
 
-; выводим сообщение, что искомых файлов не было
-mov ah, 9
-mov dx, offset mess2
-int 21h
+jmp shift_index
+
+; возвращаемся к началу элемента каталога и переходим к след. элементу
+shift_index:
+    pop si
+    add si, 32
+
+jmp next_file
+
+; проверяем, что хотя бы один файл был найден
+check_count:
+    cmp numfiles, 0
+    jne print_file_info
+
+    ; выводим сообщение, что файлов не было
+    mov ah, 9
+    mov dx, offset msg_nofiles
+    int 21h
+
+jmp exit
+
+; выводим название и размер наибольшего файла
+print_file_info:
+    ; название и расширение
+    mov ah, 9
+    mov dx, offset filename
+    int 21h
+    ; пробел
+    mov ah, 02h
+    mov dl, ' '
+    int 21h
+    ; размер
+    push filesizeh
+    call print_kb
+    pop filesizeh
+    ; переводим курсор в начало следующей строки экрана
+    mov ah, 9
+    mov dx, offset endl
+    int 21h
+
+jmp exit
 
 exit:
-; выводим сообщение, с просьбой нажать любую клавишу      
-mov ah, 9
-mov dx, offset mess3
-int 21h
+    mov ah, 4ch
+    mov al, 0
+    int 21h
 
-mov ah, 7
-int 21h                ; ждем, когда клавишу нажмут
-
-mov ah, 4ch
-int 21h
-
-end start
+end main
